d363db61abd961bd8ef086e4478675c3812444f4
meosjin <iammeosjin@gmail.com>
2024-01-23 07:58:55 +0800

    feat: disable job on off hours

--- jobs/check-triggers.ts
+++ jobs/check-triggers.ts
@@ -1,8 +1,13 @@
+// @deno-types=npm:@types/luxon
+import { DateTime } from 'npm:luxon';
 import { addJob, getJob } from '../controllers/job.ts';
 import processTriggers from '../libs/process-triggers.ts';
 import { ID } from '../types.ts';
 
 export default async function checkTriggersJob(id: ID) {
+	const now = DateTime.now();
+	if (now.hour >= 19 || now.hour < 8) return;
+	if (now.weekday > 5) return;
 	const job = await getJob(id);
 	console.log(`Running job ${id.join('-')}`, job);
 	if (!job) return;
