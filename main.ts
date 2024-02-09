5919d13e68cb6e5a2f19b773b031d54281835419
meosjin <iammeosjin@gmail.com>
2024-02-09 12:03:45 +0800

    feat: remove job

--- main.ts
+++ main.ts
@@ -1,20 +1,20 @@
-import checkTriggersJob from './jobs/check-triggers.ts';
+// import checkTriggersJob from './jobs/check-triggers.ts';
 import JobModel from './models/job.ts';
-import TriggerModel from './models/trigger.ts';
-// @deno-types=npm:@types/bluebird
-import Bluebird from 'npm:bluebird';
+// import TriggerModel from './models/trigger.ts';
+// // @deno-types=npm:@types/bluebird
+// import Bluebird from 'npm:bluebird';
 
 await JobModel.insert({
 	id: ['check-triggers'],
 	status: 'READY',
 });
 
-Deno.cron('check-triggers', '*/2 * * * *', async () => {
-	await checkTriggersJob(['check-triggers']);
-	await Bluebird.map(
-		await TriggerModel.list(),
-		(t) => TriggerModel.delete(t.id),
-	);
-});
+// Deno.cron('check-triggers', '*/2 * * * *', async () => {
+// 	await checkTriggersJob(['check-triggers']);
+// 	// await Bluebird.map(
+// 	// 	await TriggerModel.list(),
+// 	// 	(t) => TriggerModel.delete(t.id),
+// 	// );
+// });
 
 // delete task model to remove cache
